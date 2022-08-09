856c9949151f51050c2d682a21c2def30d533b48
unknown <hayounn.etd@gmail.com>
2022-08-09 19:37:59 +0000

    Update table in schema file and Update insertions in data file

--- data.sql
+++ data.sql
@@ -1,13 +1,22 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ( 'Agumon', '2020-02-03', 0, true, 10.23 );
+VALUES ( 'Charmander', '2020-02-08', 0, false, 11.0 );
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ( 'Gabumon', '2018-11-15', 2, true, 8.0 );
+VALUES ( 'Plantmon', '2021-11-15', 2, true, 5.7 );
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ( 'Pikachu', '2021-02-03', 1, false, 15.04);
+VALUES ( 'Squirtle', '1993-04-02', 3, false, 12.13);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Devimon', '2017-05-12', 5, true, 11.0);
+VALUES ('Angemon', '2005-06-12', 1, true, 45.0);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Boarmon', '2005-06-07', 7, true, 20.4);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Blossom', '1998-10-13', 3, true, 17.0);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Ditto', '2022-05-14', 4, true, 22.0);
