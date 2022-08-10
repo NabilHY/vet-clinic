c110f443081df41dcba02df8381250d070e633c9
unknown <hayounn.etd@gmail.com>
2022-08-10 21:32:29 +0100

    Insert data

--- data.sql
+++ data.sql
@@ -20,3 +20,27 @@ VALUES ('Blossom', '1998-10-13', 3, true, -17.0);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES ('Ditto', '2022-05-14', 4, true, -22.0);
+
+
+INSERT INTO owners (full_name, age)
+VALUES('Sam Smith ', 34);
+
+INSERT INTO owners (full_name, age)
+VALUES('Jennifer Orwell', 19);
+
+INSERT INTO owners (full_name, age)
+VALUES('Bob', 45);
+
+INSERT INTO owners (full_name, age)
+VALUES('Melody Pond', 77);
+
+INSERT INTO owners (id, full_name, age)
+VALUES('Jodie Whittake ', 38);
+
+INSERT INTO species (name)
+VALUES('Pokemon');
+
+INSERT INTO species (name)
+VALUES('Digimon');
+
+
