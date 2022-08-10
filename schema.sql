1efab1d85ac2111b577d617425bc7c22428dbace
unknown <hayounn.etd@gmail.com>
2022-08-10 21:21:45 +0100

    Create new tables and modify existing one

--- schema.sql
+++ schema.sql
@@ -1,11 +1,25 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(250),
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL,
-    species VARCHAR(250)
+    species_id  INT FOREIGN KEY REFERENCES species(id),
+    owner_id INT FOREIGN KEY REFERENCES owners(id),
+);
+
+CREATE TABLE owners (
+    id INT NOT NULL AUTO_INCREMENT,
+    full_name VARCHAR(250),
+    age INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(250),
+    PRIMARY KEY (id)
 );
