141e5d5ccefd20545243d7705f7d8f2f66fe041f
unknown <hayounn.etd@gmail.com>
2022-08-11 09:28:40 +0100

    Make changes

--- schema.sql
+++ schema.sql
@@ -1,25 +1,36 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(250),
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL,
-    species_id  INT FOREIGN KEY REFERENCES species(id),
-    owner_id INT FOREIGN KEY REFERENCES owners(id),
+    species VARCHAR(250)
 );
 
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD FOREIGN KEY (species_id) REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id)
+ON DELETE CASCADE;
+
+
 CREATE TABLE owners (
-    id INT NOT NULL AUTO_INCREMENT,
+    id SERIAL NOT NULL,
     full_name VARCHAR(250),
     age INT,
     PRIMARY KEY (id)
 );
 
+
 CREATE TABLE species (
-    id INT NOT NULL AUTO_INCREMENT,
+    id SERIAL NOT NULL,
     name VARCHAR(250),
     PRIMARY KEY (id)
 );
