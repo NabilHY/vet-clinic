b722a1107e97ccaeccf8bdefb8f60aee21a372c5
unknown <hayounn.etd@gmail.com>
2022-08-17 19:49:05 +0100

    Add tables from diagram

---
+++ schema_based_on_digram.sql
@@ -0,0 +1,44 @@
+CREATE TABLE medical_histories (
+    id serial NOT NULL INT ,
+    admitted_at TIMESTAMP,
+    patient_id INTEGER REFERENCES patients(id),
+    STATUS VARCHAR(250),
+    PRIMARY KEY (id),
+    FOREIGN KEY (patient_id) REFENRENCES patients(id)
+)
+
+CREATE TABLE invoice_items (
+    id serial NOT NULL,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT,
+    treatment_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+)
+
+CREATE TABLE treatements (
+    id serial NOT NULL INT,
+    type VARCHAR(250),
+    name VARCHAR(250)
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE invoices (
+    id serial NOT NULL INT,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history__id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (medical_history__id) REFERENCES medical_histories(id),
+)
+
+CREATE TABLE patients (
+    id serial NOT NULL INT,
+    name VARCHAR(250),
+    date_of_birth DATE,
+    PRIMARY KEY (id)
+)
