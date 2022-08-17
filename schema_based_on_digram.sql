2fa5b59b6b502714649f263ba25e260535a3b142
unknown <hayounn.etd@gmail.com>
2022-08-17 19:54:13 +0100

    Sort Tables

--- schema_based_on_digram.sql
+++ schema_based_on_digram.sql
@@ -1,3 +1,17 @@
+CREATE TABLE patients (
+    id serial NOT NULL INT,
+    name VARCHAR(250),
+    date_of_birth DATE,
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE treatements (
+    id serial NOT NULL INT,
+    type VARCHAR(250),
+    name VARCHAR(250)
+    PRIMARY KEY (id)
+)
+
 CREATE TABLE medical_histories (
     id serial NOT NULL INT ,
     admitted_at TIMESTAMP,
@@ -5,6 +19,16 @@ CREATE TABLE medical_histories (
     STATUS VARCHAR(250),
     PRIMARY KEY (id),
     FOREIGN KEY (patient_id) REFENRENCES patients(id)
+)
+
+CREATE TABLE invoices (
+    id serial NOT NULL INT,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history__id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (medical_history__id) REFERENCES medical_histories(id),
 )
 
 CREATE TABLE invoice_items (
@@ -19,26 +43,4 @@ CREATE TABLE invoice_items (
     FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 )
 
-CREATE TABLE treatements (
-    id serial NOT NULL INT,
-    type VARCHAR(250),
-    name VARCHAR(250)
-    PRIMARY KEY (id)
-)
 
-CREATE TABLE invoices (
-    id serial NOT NULL INT,
-    total_amount DECIMAL,
-    generated_at TIMESTAMP,
-    payed_at TIMESTAMP,
-    medical_history__id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (medical_history__id) REFERENCES medical_histories(id),
-)
-
-CREATE TABLE patients (
-    id serial NOT NULL INT,
-    name VARCHAR(250),
-    date_of_birth DATE,
-    PRIMARY KEY (id)
-)
